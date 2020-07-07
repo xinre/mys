0417af6a1125dea342f50aad4f07b34b55136605
tanghongyu <505648237@qq.com>
2020-07-07 11:00:19 +0800

    take

--- index.ts
+++ index.ts
@@ -25,7 +25,41 @@ const channel: channelType = () => {
 
 const livingChan = channel()
 
+const take = (): { type: string } => {
+  return {
+    type: 'take'
+  }
+}
+
+function* mainSaga (): IterableIterator<any> {
+  const action = yield take()
+}
+
+const task = (iterator: () => IterableIterator<any>): void => {
+  const iterVariable = iterator()
+
+  function next(args) {
+    const result = iterVariable.next(args)
+    if (!result.done) {
+      const effect = result.value
+      if (effect.type === 'take') {
+        runTakeEffect(next)
+      }
+    }
+  }
+}
+
+function runTakeEffect(next: (args?: any) => void) {
+  livingChan.take(input => {
+    next(input)
+  });
+}
+
+
+
+let count = 0
 
 function test () {
-
+  const action =`action${count++}`
+  livingChan.put(action)
 }
