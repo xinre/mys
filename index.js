d4be06659864da621f5f7528ba44d56fe7044c60
tanghongyu <505648237@qq.com>
2021-02-05 00:23:15 +0800

    tsconfig

--- index.js
+++ index.js
@@ -1,89 +1,86 @@
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-var channel = function () {
-    var taker;
-    var take = function (agu) {
+const channel = () => {
+    let taker;
+    const take = (agu) => {
         taker = agu;
     };
-    var put = function (agu) {
+    const put = (agu) => {
         if (taker) {
-            var nowTaker = taker;
+            const nowTaker = taker;
             taker = null;
             console.log(agu);
             nowTaker(agu);
         }
     };
     return {
-        put: put,
-        take: take
+        put,
+        take
     };
 };
-var livingChan = channel();
-var take = function () {
+const livingChan = channel();
+const take = () => {
     return {
         type: 'take'
     };
 };
-function mainSaga() {
-    var action;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, take()];
-            case 1:
-                action = _a.sent();
-                console.log(action, '000');
-                return [2 /*return*/];
+function* takeEvery(worker) {
+    yield fork(function* () {
+        while (true) {
+            const action = yield take();
+            worker(action);
         }
     });
 }
-var task = function (iterator) {
-    var iterVariable = iterator();
+function fork(cb) {
+    return {
+        type: 'fork',
+        fn: cb,
+    };
+}
+function runForkEffect(effect, cb) {
+    task(effect.fn || effect);
+    cb();
+}
+function* mainSaga() {
+    // const action = yield take()
+    // console.log(action, '000')
+    yield takeEvery(action => {
+        console.log(action, '66666');
+    });
+}
+const task = (iterator) => {
+    const iterVariable = iterator();
     function next(args) {
-        var result = iterVariable.next(args);
+        const result = iterVariable.next(args);
         console.log(args, result, 'pppp');
         if (!result.done) {
-            var effect = result.value;
-            if (effect.type === 'take') {
-                runTakeEffect(next);
+            const effect = result.value;
+            if (typeof effect[Symbol.iterator] === 'function') {
+                runForkEffect(effect, next);
+            }
+            else if (effect.type) {
+                switch (effect.type) {
+                    case 'take':
+                        runTakeEffect(next);
+                        break;
+                    case 'fork':
+                        runForkEffect(effect, next);
+                        break;
+                    default:
+                }
             }
         }
     }
     next();
 };
 function runTakeEffect(next) {
-    livingChan.take(function (input) {
+    livingChan.take(input => {
         next(input);
     });
 }
 task(mainSaga);
-var count = 0;
+let count = 0;
 function test() {
-    var action = "action" + count++;
+    const action = `action${count++}`;
     livingChan.put(action);
 }
 test();
